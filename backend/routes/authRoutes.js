3c9e7ec97c93dd90d85a6f1f6c8fa5a91e611875
Isha Pal <ishapal2004@gmail.com>
2026-06-12 18:46:48 +0530

    Added login route and authentication

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const {
   registerUser,
+  loginUser,
 } = require("../controller/authController");
 
 router.post("/register", registerUser);
+router.post("/login", loginUser);
 
 module.exports = router;
