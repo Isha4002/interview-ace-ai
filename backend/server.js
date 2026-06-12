76f6d64b4b73344400a6c5797ac2a6c489e3c3e3
Isha Pal <ishapal2004@gmail.com>
2026-06-12 18:32:59 +0530

    Connected auth routes to server

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,30 @@
 const dns = require("dns");
 dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
-const authRoutes = require("./routes/authRoutes");
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
+// Database Connection
 connectDB();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
+
+// Routes
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("InterviewAce API Running 🚀");
 });
 
+// Server
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
